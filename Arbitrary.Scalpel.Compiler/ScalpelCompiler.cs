0901c2ee771364c8914ee25a68cc9ae4f6174e73
Doug Ives <github@dou.gives>
2019-05-05 21:03:05 +0000

    predicate compilation

--- Arbitrary.Scalpel.Compiler/ScalpelCompiler.cs
+++ Arbitrary.Scalpel.Compiler/ScalpelCompiler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Arbitrary.Scalpel.Dissection;
 
 namespace Arbitrary.Scalpel.Compiler
@@ -87,7 +88,131 @@ namespace Arbitrary.Scalpel.Compiler
             Title = string.IsNullOrWhiteSpace(syntax.Title.Name)
                 ? throw new ArgumentException(nameof(syntax))
                 : syntax.Title.Name;
+
+            string JoinName(NameSyntax name)
+                => string.Join('.', name.Identifiers);
             
+            IEnumerable<string> FindPredicateSymbols(IPredicate ps)
+            {
+                switch (ps)
+                {
+                    case NameSyntax o:
+                        yield return JoinName(o);
+                        yield break;
+                    case UnaryOperationSyntax<IPredicate> o:
+                        foreach (var p 
+                            in FindPredicateSymbols(o.Operand))
+                            yield return p;
+                            yield break;
+                    case BinaryOperationSyntax<IPredicate, IPredicate> o:
+                        foreach (var p 
+                            in FindPredicateSymbols(o.LeftOperand))
+                            yield return p;
+                        foreach (var p 
+                            in FindPredicateSymbols(o.RightOperand))
+                            yield return p;
+                        yield break;
+                    default:
+                        yield break;
+                }
+            }
+
+            PredicateSymbols = new HashSet<string>(
+                FindPredicateSymbols(syntax.Predicate));
+
+            SelectionSymbols = new HashSet<string>(
+                syntax.Selectors.Select(s => JoinName(s.Name)));
+
+            DifferenceSymbols = new HashSet<string>(
+                PredicateSymbols.Except(SelectionSymbols));
+
+            Predicate<Dictionary<string, SymbolState>> CompilePredicate(
+                IPredicate predicate)
+            {
+                const string param_name = "symbols";
+                Type param_type = typeof(Dictionary<string, SymbolState>);
+
+                var param_var = Expression.Variable(
+                    param_type,
+                    param_name);
+
+                Expression PredicateExpression(IPredicate pe)
+                {
+                    switch (predicate)
+                    {
+                        case IntegerLiteralSyntax o:
+                            return Expression.Constant(o.Value);
+                        case StringLiteralSyntax o:
+                            return Expression.Constant(o.Text);
+                        case NameSyntax o:
+                            return Expression.Call(
+                                param_var,
+                                param_type.GetMethod("Get"),
+                                Expression.Constant(JoinName(o)));
+                        case UnaryOperationSyntax<IPredicate> o:
+                            switch (o.Type)
+                            {
+                                case UnaryOperationType.Not:
+                                    return Expression.IsFalse(
+                                        PredicateExpression(o.Operand));
+                                default:
+                                    throw new InvalidOperationException();
+                            }
+                        case BinaryOperationSyntax<IPredicate, IPredicate> o:
+                            switch (o.Type)
+                            {
+                                case BinaryOperationType.Or:
+                                    return Expression.Or(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.And:
+                                    return Expression.And(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.NotEqual:
+                                    return Expression.NotEqual(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.Equal:
+                                    return Expression.Equal(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.GreaterThanOrEqual:
+                                    return Expression.GreaterThanOrEqual(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.LessThanOrEqual:
+                                    return Expression.LessThanOrEqual(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                case BinaryOperationType.GreaterThan:
+                                    return Expression.GreaterThan(
+                                        PredicateExpression(o.LeftOperand),
+                                        PredicateExpression(o.RightOperand));
+                                default:
+                                    throw new InvalidOperationException();
+                            }
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+
+                var parameter = Expression.Parameter(
+                    param_type,
+                    param_name);
+                var lambda = Expression.Lambda(
+                    param_type,
+                    PredicateExpression(predicate),
+                    parameter);
+
+                while (lambda.CanReduce)
+                    lambda = lambda.ReduceAndCheck() as LambdaExpression;
+
+                return lambda.Compile()
+                    as Predicate<Dictionary<string, SymbolState>>;
+            }
+
+            Predicate = CompilePredicate(syntax.Predicate);
         }
     }
 }
